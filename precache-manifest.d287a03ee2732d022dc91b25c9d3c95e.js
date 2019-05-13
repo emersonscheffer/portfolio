c06e80a8005be1cde7bbbaf1e8e377c92acab4e1
Ermeson Scheffer <ermesonscheffer@Ermesons-MacBook-Pro.local>
2019-05-13 01:21:34 -0700

    Updates

---
+++ precache-manifest.d287a03ee2732d022dc91b25c9d3c95e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e799e5ed8728fe7f80d1466c2f5a55b9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bc36df1f873f15eb49d8",
+    "url": "/portfolio/static/css/main.0e403fd3.chunk.css"
+  },
+  {
+    "revision": "3580359e8a149d0e306f",
+    "url": "/portfolio/static/js/2.a9a2a434.chunk.js"
+  },
+  {
+    "revision": "bc36df1f873f15eb49d8",
+    "url": "/portfolio/static/js/main.b8b38ef6.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.784d01ab.js"
+  }
+]);
